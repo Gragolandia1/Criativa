f756419eff820e97adb98b3cb1ac5008c542f9dd
Leonardo Dias Brito <leonardobrito_2002@hotmail.com>
2024-09-23 17:30:13 -0300

    first commit

---
+++ src/main/java/com/teste/criativa/CriativaApplication.java
@@ -0,0 +1,13 @@
+package com.teste.criativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriativaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teste/criativa/CriativaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.criativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriativaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
