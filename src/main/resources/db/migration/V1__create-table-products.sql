88ffb1a0b9a548f688613a60aa2844b23c0e01dc
Leonardo Dias Brito <leonardobrito_2002@hotmail.com>
2024-09-27 17:52:29 -0300

    teste

--- src/main/resources/db/migration/V1__create-table-products.sql
+++ src/main/resources/db/migration/V1__create-table-products.sql
@@ -3,8 +3,8 @@ create table products(
 	id bigint not null auto_increment,
 	nome varchar(100) not null,
 	codigoBarras char(13) not null UNIQUE,
-	quantidade int(11) not null,
-	price decimal(10,2) not null,
+	quantidade varchar(100) not null,
+	preco varchar(100) not null,
 	fornecedor varchar(100) not null,
 	
 	primary key(id)
