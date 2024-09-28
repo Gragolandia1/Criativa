f5af02b79899da03b4fad60cb1d9f26880722350
Leonardo Dias Brito <leonardobrito_2002@hotmail.com>
2024-09-28 16:38:14 -0300

    update securityFilter

--- src/main/java/com/teste/criativa/infra/SecurityFilter.java
+++ src/main/java/com/teste/criativa/infra/SecurityFilter.java
@@ -3,6 +3,7 @@ package com.teste.criativa.infra;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -25,7 +26,7 @@ public class SecurityFilter extends OncePerRequestFilter {
     private UsuarioRepository usuarioRepository;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
             throws ServletException, IOException {
                 
                 var tokenJWT = recuperarToken(request);
--- src/main/java/com/teste/criativa/product/dtos/DadosCadastroProduto.java
+++ src/main/java/com/teste/criativa/product/dtos/DadosCadastroProduto.java
@@ -2,10 +2,8 @@ package com.teste.criativa.product.dtos;
 
 import com.teste.criativa.product.enums.Fornecedor;
 import jakarta.persistence.Enumerated;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
