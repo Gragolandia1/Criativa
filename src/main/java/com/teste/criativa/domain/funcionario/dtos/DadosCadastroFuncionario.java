533b784decb1759cceb90141c2a00c4c045446e6
Leonardo Dias Brito <leonardobrito_2002@hotmail.com>
2024-10-01 22:52:16 -0300

    Reformat code

--- src/main/java/com/teste/criativa/domain/funcionario/dtos/DadosCadastroFuncionario.java
+++ src/main/java/com/teste/criativa/domain/funcionario/dtos/DadosCadastroFuncionario.java
@@ -1,38 +1,36 @@
 package com.teste.criativa.domain.funcionario.dtos;
 
 import com.teste.criativa.domain.funcionario.enums.Sexo;
-
-import org.hibernate.validator.constraints.br.CPF;
-
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.br.CPF;
 
 public record DadosCadastroFuncionario(
-		
-		@NotBlank
-		@Size(min = 3, message = "O nome deve ter no minimo 3 caracteres")
-		@Size(max = 20, message = "O nome deve ter no maximo 20 caracteres")
-	    String nome,
-	    
-	    @NotBlank
-	    @Size(min = 3, message = "O sobrenome deve ter no minimo 3 caracteres")
-		@Size(max = 30, message = "O sobrenome deve ter no maximo 30 caracteres")
-		String sobrenome,
-		
-		@CPF
-		String cpf,
-		 
-		@Enumerated
-		@NotNull(message = "O genero nao pode ser nulo")
+
+        @NotBlank
+        @Size(min = 3, message = "O nome deve ter no minimo 3 caracteres")
+        @Size(max = 20, message = "O nome deve ter no maximo 20 caracteres")
+        String nome,
+
+        @NotBlank
+        @Size(min = 3, message = "O sobrenome deve ter no minimo 3 caracteres")
+        @Size(max = 30, message = "O sobrenome deve ter no maximo 30 caracteres")
+        String sobrenome,
+
+        @CPF
+        String cpf,
+
+        @Enumerated
+        @NotNull(message = "O genero nao pode ser nulo")
         Sexo sexo,
 
-		@NotBlank
-		@Min(value = 18, message = "Idade invalida")
-		String idade
-		
-		 ){
+        @NotBlank
+        @Min(value = 18, message = "Idade invalida")
+        String idade
+
+) {
 
 }
--- src/main/java/com/teste/criativa/domain/product/dtos/DadosCadastroProduto.java
+++ src/main/java/com/teste/criativa/domain/product/dtos/DadosCadastroProduto.java
@@ -2,34 +2,30 @@ package com.teste.criativa.domain.product.dtos;
 
 import com.teste.criativa.domain.product.enums.Fornecedor;
 import jakarta.persistence.Enumerated;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
+
 import java.math.BigDecimal;
 
 public record DadosCadastroProduto(
-		
-		@NotBlank
-		@Size(max = 50, message = "O nome não pode ter mais de 50 caracteres")
-		String nome,
-		
-		@NotBlank
-		@Pattern(regexp = "\\d{6,13}", message = "Formato do codigo de barras é inválido. Deve conter entre 6 e 13 dígitos")
-		String codigoBarras,
-		
-		@Min(value = 1, message = "A quantidade deve ser maior que zero")
-		@NotNull
-		Integer quantidade,
-		
-		@Positive
-		@NotNull
-		BigDecimal preco,
-		
-		@Enumerated
-		@NotNull(message = "O fornecedor não pode ser nulo")
-		Fornecedor fornecedor) {	
+
+        @NotBlank
+        @Size(max = 50, message = "O nome não pode ter mais de 50 caracteres")
+        String nome,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{6,13}", message = "Formato do codigo de barras é inválido. Deve conter entre 6 e 13 dígitos")
+        String codigoBarras,
+
+        @Min(value = 1, message = "A quantidade deve ser maior que zero")
+        @NotNull
+        Integer quantidade,
+
+        @Positive
+        @NotNull
+        BigDecimal preco,
+
+        @Enumerated
+        @NotNull(message = "O fornecedor não pode ser nulo")
+        Fornecedor fornecedor) {
 
 }
